327d7fb4c72dd8a7f6ef08a3078e0d9d6f8f7104
Juliette Xie <juliette.xie@epitech.eu>
2019-01-21 01:56:07 +0100

    Added interval hoc

---
+++ src/hoc/withInterval.js
@@ -0,0 +1,12 @@
+import { lifecycle } from 'recompose';
+
+export default (ft, delay) => lifecycle({
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      ft(this.props);
+    }, delay);
+  },
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  },
+});
