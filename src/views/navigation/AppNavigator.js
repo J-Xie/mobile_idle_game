4b245d6598f76b7cef7a74f8daab9b662d376a5b
Juliette Xie <juliette.xie@epitech.eu>
2019-01-21 02:01:03 +0100

    Added Settings route/screen to App drawer

--- src/views/navigation/AppNavigator.js
+++ src/views/navigation/AppNavigator.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { createDrawerNavigator, createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from 'react-navigation';
 
-// import Homepage from '../../../App';
 import Homepage from '../Homepage';
 import ResourceList from '../ResourceListView';
 import Income from '../Income';
+import Settings from '../Settings';
 
-console.log(Homepage)
+console.log(Homepage);
 
 export default createDrawerNavigator({
-    Home: {screen: Homepage},
-    Resources: {screen: ResourceList},
-    Income: {screen: Income},
-  },
+  Home: { screen: Homepage },
+  Resources: { screen: ResourceList },
+  Income: { screen: Income },
+  Settings: { screen: Settings },
+},
   // {
   //   contentComponent: props => <SideBar {...props} />,
   // },
