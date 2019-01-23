042f4a44f5a79ea437ea98de777731ce00b05cb8
Juliette Xie <juliette.xie@epitech.eu>
2019-01-23 03:36:00 +0100

    Updated App component with theme variable

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import {
-  compose, withState, withHandlers, onlyUpdateForPropTypes, lifecycle,
-} from 'recompose';
+import PropTypes from 'prop-types';
+import { compose, withState, onlyUpdateForPropTypes } from 'recompose';
 import { connect } from 'react-redux';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import {
-  Container, Content, Drawer, Button, Text,
-} from 'native-base';
+import { Container, Button } from 'native-base';
 
 import AppHeader from './navigation/Header';
-import Sidebar from './navigation/Sidebar';
+import { Text } from './components';
 
 import { selectTheme } from '../redux/settings/selector';
 
 const styles = EStyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '$bgColor',
   },
   button: {
     paddingLeft: 15,
@@ -25,33 +23,35 @@ const styles = EStyleSheet.create({
   },
 });
 
-const Homepage = ({
-  setDrawer, openDrawer, closeDrawer, navigation,
-}) => (
-  console.log('azeaze')
-  || (
-  <Container style={styles.container}>
-    <Drawer
+const Homepage = ({ navigation, themeKey }) => (
+  <Container style={styles.container} key={themeKey}>
+    {/* <Drawer
       ref={(ref) => { setDrawer(ref); }}
       content={<Sidebar navigator={navigation} />}
       onClose={closeDrawer}
+    > */}
+    <AppHeader navigation={navigation} />
+    <Button
+      bordered
+      info
+      style={styles.button}
+      onPress={() => console.log(navigation)}
     >
-      <AppHeader navigation={navigation} />
-      <Button bordered info style={styles.button} onPress={() => console.log(navigation)}>
-        <Text>Owned resources</Text>
-      </Button>
-      <Button bordered info>
-        <Text>Info</Text>
-      </Button>
-      <Button onPress={() => navigation.navigate('Resources')}>
-        <Text>List</Text>
-      </Button>
-    </Drawer>
+      <Text>Owned resources</Text>
+    </Button>
+    <Button bordered info>
+      <Text>Info</Text>
+    </Button>
+    <Button onPress={() => navigation.navigate('Resources')}>
+      <Text>List</Text>
+    </Button>
+    {/* </Drawer> */}
   </Container>
-  )
 );
 
-Homepage.propTypes = {};
+Homepage.propTypes = {
+  theme: PropTypes.string.isRequired,
+};
 
 const mapStateToProps = state => ({
   theme: selectTheme(state),
@@ -60,16 +60,15 @@ const mapStateToProps = state => ({
 export default compose(
   connect(mapStateToProps),
   withState('drawer', 'setDrawer', null),
-  lifecycle({
-    componentDidMount() {
-      const theme = this.props.theme ? '' : '';
-      EStyleSheet.build(theme);
-    },
-  }),
-  // withHandlers({
-  // openDrawer: props => () => props.navigation.openDrawer(),
-  // closeDrawer: props => () => props.navigation.closeDrawer(),
-  // closeDrawer: props => () => props.drawer._root.close(),
+  withState('themeKey', 'update'),
+  // withPropsOnChange(['theme'], ({ theme: themeName, update }) => {
+  //   console.log(theme);
+  //   if (!theme[themeName]) {
+  //     console.warn('Unknown theme :', themeName);
+  //   } else {
+  //     EStyleSheet.build(theme[themeName]);
+  //   }
+  //   update(themeName);
   // }),
-  onlyUpdateForPropTypes,
+  onlyUpdateForPropTypes
 )(Homepage);
