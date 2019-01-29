28a9d66a1a785c91459f2c96c102881a1e9dabc1
Juliette Xie <juliette.xie@epitech.eu>
2019-01-29 21:30:21 +0100

    divider exported

--- src/views/components/index.js
+++ src/views/components/index.js
@@ -3,3 +3,4 @@ export { default as Button } from './Button';
 export { default as Text } from './Text';
 export { default as Tabs } from './Tabs';
 export { default as ProgressButton } from './ProgressButton';
+export { default as Divider } from './Divider';
