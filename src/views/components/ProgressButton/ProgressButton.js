7da55ba781d980cb2399596c02840c5384e587ff
Juliette <juliette.xie@epitech.eu>
2019-02-24 15:00:47 +0100

    Moved css into EStyleSheet

--- src/views/components/ProgressButton/ProgressButton.js
+++ src/views/components/ProgressButton/ProgressButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import EStyleSheet from 'react-native-extended-stylesheet';
 import { View, TouchableOpacity } from 'react-native';
 import {
   compose,
@@ -12,42 +13,43 @@ import Text from '../Text';
 
 import withControlledInterval from '../../../hoc/withControllerInterval';
 
+const styles = EStyleSheet.create({
+  progressButton: {
+    marginTop: 10,
+    minHeight: 50,
+    minWidth: 100,
+    width: 250,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '$textColor',
+  },
+  progressContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  progressBar: {
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    height: 5,
+    backgroundColor: 'green',
+  },
+  content: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 const Progress = ({ percent, children }) => (
-  <View
-    style={{
-      marginTop: 10,
-      minHeight: 50,
-      minWidth: 100,
-      width: 250,
-      borderRadius: 5,
-      borderWidth: 1,
-      borderColor: 'white',
-    }}
-  >
+  <View style={styles.progressButton}>
     {percent < 1 && (
-      <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-        <View
-          style={{
-            borderBottomRightRadius: 5,
-            borderBottomLeftRadius: 5,
-            width: `${percent * 100}%`,
-            height: 5,
-            backgroundColor: 'green',
-          }}
-        />
+      <View style={styles.progressContainer}>
+        <View style={{ ...styles.progressBar, width: `${percent * 100}%` }} />
       </View>
     )}
-    <View
-      style={{
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      {children}
-    </View>
+    <View style={styles.content}>{children}</View>
   </View>
 );
 Progress.propTypes = {
