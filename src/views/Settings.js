9ac9f0ab105e38d832639437fa23b8af338fa65d
Juliette <juliette.xie@epitech.eu>
2019-02-24 14:42:42 +0100

    Fixed Settings max logs edition

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Alert } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { Content, Grid, Col, Row } from 'native-base';
-import { compose, withHandlers } from 'recompose';
+import { compose, withHandlers, withState, withPropsOnChange } from 'recompose';
 import { connect } from 'react-redux';
 import { changeTheme, reset } from '../redux/settings/action';
 import { selectTheme } from '../redux/settings/selector';
@@ -42,59 +42,72 @@ const styles = EStyleSheet.create({
   input: { flex: 1, textAlign: 'center' },
 });
 
-const Settings = ({ maxLogs, theme, setMaxLogs, setTheme, validateWipe }) => (
-  <Content style={styles.container}>
-    <Text style={styles.title}>Main settings</Text>
-    <Grid>
-      <Row style={styles.row}>
-        <Col style={styles.colTitle}>
-          <Text>Change maximum log number</Text>
-          {/* <Text>Defines the number of log displayed.</Text> */}
-        </Col>
-        <Col style={styles.content}>
-          {maxLogs && (
+const Settings = ({
+  maxLogs,
+  theme,
+  setMaxLogs,
+  setTheme,
+  validateWipe,
+  logs,
+  setLogs,
+}) =>
+  console.log(maxLogs) || (
+    <Content style={styles.container}>
+      <Text style={styles.title}>Main settings</Text>
+      <Grid>
+        <Row style={styles.row}>
+          <Col style={styles.colTitle}>
+            <Text>Change maximum log number</Text>
+            {/* <Text>Defines the number of log displayed.</Text> */}
+          </Col>
+          <Col style={styles.content}>
             <TextInput
               style={styles.input}
-              value={maxLogs.toString()}
-              placeholder={`${maxLogs}`}
+              value={logs || '0'}
               keyboardType="number-pad"
-              onChangeText={value => setMaxLogs(Number(value))}
+              onChangeText={value => {
+                setLogs(value);
+                setMaxLogs(value);
+                if (!value) {
+                  setLogs('0');
+                }
+              }}
+              placeholder=""
             />
-          )}
-        </Col>
-      </Row>
-      <Divider style={{ flex: 1, justifyContent: 'center' }} />
-      <Row style={styles.row}>
-        <Col style={styles.colTitle}>
-          <Text>Change current theme</Text>
-          {/* <Text>Changes the main colors of the app.</Text> */}
-        </Col>
-        <Col style={styles.content}>
-          <Button
-            onPress={() => setTheme('light')}
-            text="light"
-            selected={theme === 'light'}
-          />
-          <Button
-            onPress={() => setTheme('dark')}
-            text="dark"
-            selected={theme === 'dark'}
-          />
-        </Col>
-      </Row>
-      <Divider />
-      <Row style={styles.row}>
-        <Col style={styles.colTitle}>
-          <Text>Wipe data</Text>
-          {/* <Text>Delete current save.</Text> */}
-        </Col>
-        <Col style={styles.content}>
-          <Button text="Wipe data" onPress={validateWipe} />
-        </Col>
-      </Row>
-    </Grid>
-  </Content>
-);
+          </Col>
+        </Row>
+        <Divider style={{ flex: 1, justifyContent: 'center' }} />
+        <Row style={styles.row}>
+          <Col style={styles.colTitle}>
+            <Text>Change current theme</Text>
+            {/* <Text>Changes the main colors of the app.</Text> */}
+          </Col>
+          <Col style={styles.content}>
+            <Button
+              onPress={() => setTheme('light')}
+              text="light"
+              selected={theme === 'light'}
+            />
+            <Button
+              onPress={() => setTheme('dark')}
+              text="dark"
+              selected={theme === 'dark'}
+            />
+          </Col>
+        </Row>
+        <Divider />
+        <Row style={styles.row}>
+          <Col style={styles.colTitle}>
+            <Text>Wipe data</Text>
+            {/* <Text>Delete current save.</Text> */}
+          </Col>
+          <Col style={styles.content}>
+            <Button text="Wipe data" onPress={validateWipe} />
+          </Col>
+        </Row>
+      </Grid>
+    </Content>
+  );
 
 const mapStateToProps = state => ({
   maxLogs: selectMaxLogs(state),
@@ -112,6 +125,7 @@ export default compose(
     mapStateToProps,
     mapDispatchToProps
   ),
+  withState('logs', 'setLogs', ''),
   withHandlers({
     validateWipe: props => () => {
       Alert.alert(
@@ -128,5 +142,8 @@ export default compose(
         { cancelable: false }
       );
     },
+  }),
+  withPropsOnChange(['maxLogs'], ({ maxLogs, setLogs }) => {
+    setLogs(maxLogs.toString());
   })
 )(Settings);
